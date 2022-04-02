f8e47651f3b015b8b26f3d15f287f0c1289efd6e
Nesto <tanlong6121@gmail.com>
2022-04-03 03:19:39 +0700

    update

--- js/app.js
+++ js/app.js
@@ -10,4 +10,22 @@ $(document).ready(function(){
         nav: true,
         navText : ["<i class='fas fa-angle-left'></i>","<i class='fas fa-angle-right'></i>"]
     });
+    $('.tm-carousel').flickity({
+        draggable: true,
+        wrapAround: true,
+        prevNextButtons: true,
+      });
 });
+
+$(".collapse-header").click(function(){
+    $(this).toggleClass("active-content");
+    $(this).next().toggle();
+});
+
+$(".regis-tab").click(function(){
+    $(".regis-tab").removeClass("active");
+    $(this).addClass("active");
+    var $target=$(this).data("target");
+    $(".regis-tab-content").addClass("inactive-tab");
+    $($target).removeClass("inactive-tab");
+})
